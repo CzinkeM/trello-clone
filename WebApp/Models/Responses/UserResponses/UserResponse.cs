617f264f6f426400ebe7a54cfdd5451406e19532
CzinkeM <mczinke@protonmail.com>
2022-09-07 14:17:17 +0200

    Created user specific responses.

---
+++ WebApp/Models/Responses/UserResponses/UserResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models.Responses.UserResponse
+{
+    public class UserResponse
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+    }
+}
