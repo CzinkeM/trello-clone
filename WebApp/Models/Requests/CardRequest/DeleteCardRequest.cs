49e3441c978ce173c6f7c8e2e5c3e32ed575409f
CzinkeM <mczinke@protonmail.com>
2022-09-07 14:16:10 +0200

    Created classes for the card requestes.

---
+++ WebApp/Models/Requests/CardRequest/DeleteCardRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models.Requests.CardRequest
+{
+    public class DeleteCardRequest
+    {
+        public DeleteCardRequest(Guid id)
+        {
+            Id = id;
+        }
+
+        [JsonPropertyName("Id")]
+        public Guid Id { get; set; }
+    }
+}
---
+++ WebApp/Models/Requests/CardRequest/UpdateCardRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models.Requests.CardRequest
+{
+    public class UpdateCardRequest
+    {
+        [JsonPropertyName("id")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("position")]
+        public int Position { get; set; }
+
+        [JsonPropertyName("asigneeId")]
+        public Guid AsigneeId { get; set; }
+    }
+}
