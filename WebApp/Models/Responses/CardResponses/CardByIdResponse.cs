868da8417b6c97ad6d12054bbe9fef043d959c32
CzinkeM <mczinke@protonmail.com>
2022-09-07 14:16:51 +0200

    Created card specific responses.

---
+++ WebApp/Models/Responses/CardResponses/CardByIdResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models.Responses.CardResponse
+{
+    public class CardByIdResponse
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public int position { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string OwnerId { get; set; }
+        public string AsigneeId { get; set; }
+    }
+}
---
+++ WebApp/Models/Responses/CardResponses/SimpleCardResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models.Responses.CardResponse
+{
+    public class SimpleCardResponse
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public int Position { get; set; }
+
+    }
+}
