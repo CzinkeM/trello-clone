c3811c4c539e6ec1ce6ea468bfae0d28959b180a
CzinkeM <mczinke@protonmail.com>
2022-09-07 14:16:28 +0200

    Created user specific requests.

---
+++ WebApp/Models/Requests/UserRequest/SignInRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.Models.Requests.UserRequest
+{
+    public class SignInRequest
+    {
+        public SignInRequest(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+    }
+}
