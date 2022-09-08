ef8da3bdedaad618e6355e82237c1a38a443bb72
CzinkeM <mczinke@protonmail.com>
2022-09-08 09:38:18 +0200

    Created mapper profiles.

---
+++ WebApp/Models/Mappers/UserModelToRequestProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebApp.Models.Requests.UserRequest;
+
+namespace WebApp.Models.Mappers
+{
+    public class UserModelToRequestProfile : Profile
+    {
+        public UserModelToRequestProfile()
+        {
+            CreateMap<UserCredentials, SignInRequest>();
+            CreateMap<UserCredentials, SignUpRequest>();
+        }
+    }
+}
---
+++ WebApp/Models/Mappers/UserResponseToModelProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebApp.Models.Responses.UserResponse;
+
+namespace WebApp.Models.Mappers
+{
+    public class UserResponseToModelProfile : Profile
+    {
+        public UserResponseToModelProfile()
+        {
+            CreateMap<UserResponse, User>();
+            CreateMap<UserListResponse, UserList>();
+        }
+    }
+}
